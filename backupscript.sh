33af27568e0907a407b4510898380fe3edd17611
kunal <knlrmtk@gmail.com>
2024-08-02 10:13:37 +0530

    commit v3

--- backupscript.sh
+++ backupscript.sh
@@ -5,3 +5,4 @@ bkp_path = `mkdir -p /home/backups/project_date %d%b%Y`
 
 mysqldump -u root db_name > $bkp_path
 tar -cvzf $bkp_path.tar.gz $bkp_path
+scp -r $bkp_path.tar.gz 192.169.10.10:/home/allbackups
